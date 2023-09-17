c261c95573460e161342e7df6fe7dd3207fa7635
Ellina Babaian <famogl375@gmail.com>
2023-09-17 12:10:54 -0700

    Setting up project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Features from "./components/features/Features";
+import Discover from "./components/discover/Discover";
+import Book from "./components/book/Book";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Header />
+      <Discover />
+      <Features />
+      <Book />
+      <Footer />
     </div>
   );
 }
