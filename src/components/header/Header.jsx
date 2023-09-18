f1c1c5d9bff8703a6bd288452271394481f375a9
Ellina Babaian <famogl375@gmail.com>
2023-09-17 20:07:42 -0700

    clean up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-import "./rain"
+import "./rain";
 
 const Header = () => {
   document.addEventListener("mousemove", (e) => {
@@ -10,47 +10,52 @@ const Header = () => {
     });
   });
 
+
   return (
-    <div className="header__body">
-      <div
-        className="logo"
-        style={{
-          backgroundImage:
-            "url(https://static.wildsouls.gr/app/uploads/2020/11/logo-1.png)",
-        }}
-      ></div>
-      <section className="layers">
-        <div className="layers__container">
+    <div className="wrapper">
+      <div className="content">
+        <div className="header__body">
           <div
-            className="layers__item layer-1"
-            style={{ backgroundImage: "url(img/layer-1.jpg)" }}
+            className="logo"
+            style={{
+              backgroundImage:
+                "url(https://static.wildsouls.gr/app/uploads/2020/11/logo-1.png)",
+            }}
           ></div>
-          <div
-            className="layers__item layer-2"
-            style={{ backgroundImage: "url(img/layer-2.png)" }}
-          ></div>
-          <div className="layers__item layer-3">
-            <div className="header-content">
-              <h1 className="header-content__h1">Wild Soul</h1>
-              <div className="header-content__p">
-                Nature Retreats & Adventures
+          <section className="layers">
+            <div className="layers__container">
+              <div
+                className="layers__item layer-1"
+                style={{ backgroundImage: "url(img/layer-1.jpg)" }}
+              ></div>
+              <div
+                className="layers__item layer-2"
+                style={{ backgroundImage: "url(img/layer-2.png)" }}
+              ></div>
+              <div className="layers__item layer-3">
+                <div className="header-content">
+                  <h1 className="header-content__h1">Wild Soul</h1>
+                  <div className="header-content__p">
+                    Nature Retreats & Adventures
+                  </div>
+                  <button className="button__start">Start Exploring</button>
+                </div>
               </div>
-              <button className="button__start">Start Exploring</button>
+              <div className="layers__item layer-4">
+                <canvas className="rain"></canvas>
+              </div>
+              <div
+                className="layers__item layer-5"
+                style={{ backgroundImage: "url(img/layer-5.png)" }}
+              ></div>
+              <div
+                className="layers__item layer-6"
+                style={{ backgroundImage: "url(img/layer-6.png)" }}
+              ></div>
             </div>
-          </div>
-          <div className="layers__item layer-4">
-            <canvas className="rain"></canvas>
-          </div>
-          <div
-            className="layers__item layer-5"
-            style={{ backgroundImage: "url(img/layer-5.png)" }}
-          ></div>
-          <div
-            className="layers__item layer-6"
-            style={{ backgroundImage: "url(img/layer-6.png)" }}
-          ></div>
+          </section>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
