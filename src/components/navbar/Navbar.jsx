3eb8ce54f0d6700ce9157b780a0f1983192af46a
Ellina Babaian <famogl375@gmail.com>
2023-09-18 21:24:26 -0700

    adding an onscroll functionality

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,23 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = (props) => {
+  window.onscroll = () => {scrollNavbar()}
+  const scrollNavbar = () => {
+    const navbar = document.getElementsByClassName("navbar__body");
+    const links = document.getElementsByClassName("navbar__container");
+  
+    if (document.documentElement.scrollTop > 700) {
+      // Iterate over each link element and add the class to each one.
+      for (let i = 0; i < links.length; i++) {
+        links[i].classList.add("scrolled_item");
+      }
+    } else {
+      for (let i = 0; i < links.length; i++) {
+        links[i].classList.remove("scrolled_item");
+      }
+    }
+  };
+
   return (
     <div className="navbar__body">
       <ul className="navbar__container">
