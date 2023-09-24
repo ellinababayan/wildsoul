a0927a5e12743871364120525cc635c33e0c31d9
Ellina Babaian <famogl375@gmail.com>
2023-09-23 19:59:38 -0700

    adding scroll feature

--- src/components/gallery/Plans.jsx
+++ src/components/gallery/Plans.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Plans.css";
 
-const Plans = () => {
+const Plans = (props) => {
   return (
     <div
       className="plans__body"
@@ -50,7 +50,7 @@ const Plans = () => {
                 </li>
               </span>
             </p>
-            <button className="btn" >
+            <button className="btn">
               <span className="btn__visible">Book now</span>
               <span className="btn__invisible">270$</span>
             </button>
@@ -80,7 +80,7 @@ const Plans = () => {
                 </li>
               </span>
             </p>
-            <button className="btn" >
+            <button className="btn">
               <span className="btn__visible">Book now</span>
               <span className="btn__invisible">350$</span>
             </button>
@@ -106,12 +106,41 @@ const Plans = () => {
                 </li>
               </span>
             </p>
-            <button className="btn" >
+            <button className="btn">
               <span className="btn__visible">Book now</span>
               <span className="btn__invisible">580$</span>
             </button>
           </div>
         </section>
+        <a
+          className="scroll_down"
+          onClick={(e) => {
+            e.preventDefault();
+            props.scrollToSection("book");
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 60"
+            x="0px"
+            y="0px"
+            className="scroll_down-svg"
+          >
+            <title>ui-dan-edition_final_200106_outlined</title>
+            <path
+              d="M27,10.32v2.06a12,12,0,1,1-4-.33v12l-2.19-2.19L19.4,23.28l3.53,3.54a1.48,1.48,0,0,0,2.06.05l.06-.05,3.54-3.54-1.41-1.41-.18.18-2,2V8H23v2.05a13.93,13.93,0,1,0,4,.27Z"
+              fill="#5f9b6d"
+            />
+            <text
+              x="0"
+              y="68"
+              fill="rgb(4, 79, 15)"
+              fontSize="5px"
+              fontWeight="bold"
+              fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+            ></text>
+          </svg>
+        </a>
       </div>
     </div>
   );
